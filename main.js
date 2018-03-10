993ee00a85b7093ffd787a8edf54c6759fbe5610
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-10 13:28:26 -0600

    added variables and objects

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 console.log("hello");
+
+
+
+// Objects - Languages
+
+var spanish = {
+    "merry":"Feliz", 
+    "christmas":"Navidad", 
+    "and":"y", 
+    "happy":"Próspero", 
+    "new":"Neuvo", 
+    "year":"Año"
+};
+
+var german = {
+    "merry":"Fröhlich", 
+    "christmas":"Weihnachten", 
+    "and":"und", 
+    "happy":"Glücklich", 
+    "new":"Neu", 
+    "year":"Jahr"
+};
+
+var esperanto = {
+    "merry":"Feliĉan", 
+    "christmas":"Kristnaskon", 
+    "and":"und", 
+    "happy":"Glücklich", 
+    "new":"Neu", 
+    "year":"Jahr"
+};
+
+var userType = document.getElementById("input").value;
+
+var english = userType.split(" ");
+
+console.log(spanish, german, esperanto, english);
