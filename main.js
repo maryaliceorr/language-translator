51e1895669247f1a74c191e91a31194bf6b550ec
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-11 19:45:57 -0500

    added more styling

--- main.js
+++ main.js
@@ -31,8 +31,63 @@ var esperanto = {
     "year":"Jahr"
 };
 
-var userType = document.getElementById("input").value;
+var userInput = document.getElementById("input").value;
 
-var english = userType.split(" ");
+var inputString = userInput.toLowerCase().split(" ");
 
-console.log(spanish, german, esperanto, english);
+var newString = "";
+
+// for (var inputString in spanish) {
+    
+
+}
+
+// console.log(spanish, german, esperanto, inputString);
+
+
+
+
+
+// function buttonClick 
+
+
+// function matchLanguage (english, spanish) {
+//     for (var i=0; i<english; i++) {
+//         for (var i=0; i<spanish; i++) {
+//             if (english[i] === spanish[i]) {
+//             console.log(spanish);
+//         }
+//             else {console.log(nope)};
+//             }
+//         }
+// };
+
+
+matchLanguage.call();
+
+// var spanishButton = document.getElementById("spanish-button");
+
+// spanishButton.addEventListener("click", buttonClick);
+
+// function buttonClick() {
+//     alert ("Hello World!");
+// }
+
+
+// function 
+
+// function match ()
+
+
+var string1 = "";
+var object1 = {a: 1, b: 2, c: 3};
+
+for (var property1 in object1) {
+  string1 = string1 + object1[property1];
+}
+
+console.log(string1);
+// expected output: "123"
+
+
+
